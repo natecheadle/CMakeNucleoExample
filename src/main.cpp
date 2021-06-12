77bf8fd4ca83672ed3ae52c994bc4e7e99c4d405
natecheadle_school <natecheadle@users.noreply.github.com>
2021-06-11 17:19:48 -0700

    fixed interrupt function issue.

--- src/main.cpp
+++ src/main.cpp
@@ -218,7 +218,7 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
-void EXTI15_10_IRQHandler(void)
+extern "C" void EXTI15_10_IRQHandler(void)
 {
   __HAL_GPIO_EXTI_CLEAR_IT(B1_Pin);
 
